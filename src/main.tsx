b765f4b2dffd77a2dc26e35d6c4f306466cf8c0d
Rathod <rathod74165@gmail.com>
2025-08-29 23:33:00 +0530

    new speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
 import './index.css'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
+    <SpeedInsights />
   </React.StrictMode>
 );
