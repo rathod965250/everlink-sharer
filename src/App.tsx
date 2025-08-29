51d9251b242e75f7d598bc0e09ed2d9c64d2ea37
Rathod <rathod74165@gmail.com>
2025-08-29 23:09:52 +0530

    speed insights code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
@@ -31,6 +32,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <Analytics />
+      <SpeedInsights />
       <BrowserRouter>
         <AuthProvider>
           <Routes>
